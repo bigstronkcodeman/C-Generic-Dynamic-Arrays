baab86db8da4ab7ce839a2a6e5c395f4cf484558
bigstronkcodeman <43645849+bigstronkcodeman@users.noreply.github.com>
2022-08-15 16:13:21 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "dynarray.h"
-
+ 
+// example struct
 struct A {
     char x;
     float y;
@@ -14,26 +15,29 @@ int main() {
     struct DynArr da4;
     struct DynArr da5;
 
+    // initialize the containers with their respective underlying types
     ar_init(da, double);
     ar_init(da2, short);
     ar_init(da3, struct A);
     ar_init(da4, int[10][10]);
-    ar_init(da5, struct DynArr, copyda, destroyda);
+    ar_init(da5, struct DynArr, copyda, destroyda); // copy constructor and destructor optional, for memory-owning types
 
+    // push some rvalues into arrays
     ar_pushrv(da, 1454.2564);
     ar_pushrv(da, 94.333333);
     ar_pushrv(da, 87941.4);
-    ar_pushrv(da, 5349.15344);
+    ar_pushrv(da, 5349.15344);    
+    ar_pushrv(da2, 4987);
+    ar_pushrv(da2, 165);
+    ar_pushrv(da2, 6666);
 
+    // insert and remove some items from da
     ar_insertrv(da, 567.765, 1);
     ar_insertrv(da, 1234.5678, 2);
     ar_remove(da, 5);
     ar_remove(da, 0);
 
-    ar_pushrv(da2, 4987);
-    ar_pushrv(da2, 165);
-    ar_pushrv(da2, 6666);
-
+    // initialize a struct and push it onto an array
     struct A a = {
             .x = 'X',
             .y = 3.14159f,
@@ -45,6 +49,7 @@ int main() {
     ar_push(da3, a);
     ar_pushrv(da3, ((struct A){ .x = 'W', .y = 1.1111111, .z = 49395 }));
 
+    // initialize and add 2D arrays to a dynamic array
     int arr[10][10] = { {1,2,3,4,5,6,7,8,9,10},
                         {1,2,3,4,5,6,7,8,9,10},
                         {1,2,3,4,5,6,7,8,9,10},
@@ -60,28 +65,34 @@ int main() {
     ar_push(da4, arr); arr[2][2] = 333;
     ar_push(da4, arr);
 
+    // push removed values back onto an array
     ar_push(da5, da); ar_pushrv(da, *(double*)ar_get(da, da.len-1) + 1);
     ar_push(da5, da); ar_pushrv(da, *(double*)ar_get(da, da.len-1) + 1);
     ar_push(da5, da); ar_pushrv(da, *(double*)ar_get(da, da.len-1) + 1);
     ar_push(da5, da); ar_pushrv(da, *(double*)ar_get(da, da.len-1) + 1);
 
+    // iterate over and print a dynamic array of floats
     for (size_t i = 0; i < da.len; i++) {
         double* item = ar_get(da, i);
         printf("%lf\n", *item);
     } printf("\n");
 
+    // free a dynamic array
     ar_del(da);
 
+    // iterate over and print a dynamic array of shorts
     for (size_t i = 0; i < da2.len; i++) {
         short* item = ar_get(da2, i);
         printf("%d\n", *item);
     } printf("\n");
 
+    // iterate over and print a dynamic array of custom type
     for (int i = 0; i < da3.len; i++) {
         struct A* item = ar_get(da3, i);
         printf("%c %f %llu\n", item->x, item->y, item->z);
     } printf("\n");
 
+    // iterate over and print 2D arrays stored in dynamic array
     for (size_t i = 0; i < da4.len; i++) {
         int (*item)[10][10] = ar_get(da4, i);
         for (size_t j = 0; j < 10; j++) {
@@ -93,6 +104,7 @@ int main() {
         printf("\n");
     }
 
+    // iterate over and print a nested dynamic array (a dynamic array of dynamic arrays)
     for (size_t i = 0; i < da5.len; i++) {
         struct DynArr* item = ar_get(da5, i);
         for (size_t j = 0; j < item->len; j++) {
@@ -102,6 +114,7 @@ int main() {
         printf("\n");
     } printf("\n");
 
+    // free some more arrays
     ar_del(da2);
     ar_del(da3);
     ar_del(da4);
@@ -111,10 +124,12 @@ int main() {
     struct DynArr da7;
     struct DynArr da8;
 
+    // initialize new dynamic arrays of memory-owning types
     ar_init(da6, struct DynArr, copyouterda, destroyouterda);
     ar_init(da7, struct DynArr, copyda, destroyda);
     ar_init(da8, int);
 
+    // push and pop some values
     ar_pushrv(da8, 1); ar_pushrv(da8, 2); ar_pushrv(da8, 3);
     ar_push(da7, da8); ar_push(da6, da7); ar_pushrv(da8, 4);
     ar_push(da7, da8); ar_push(da6, da7); ar_pushrv(da8, 5);
@@ -122,7 +137,7 @@ int main() {
     ar_push(da7, da8); ar_push(da6, da7);
     ar_remove(da6, 1);
 
-
+    // iterate over and print a 3D dynamic array (dynamic array of dynamic arrays of dynamic arrays)
     for (size_t i = 0; i < da6.len; i++) {
         struct DynArr* item = ar_get(da6, i);
         for (size_t j = 0; j < item->len; j++) {
@@ -136,6 +151,7 @@ int main() {
         printf("\n");
     }
 
+    // free the inner arrays
     for (size_t i = 0; i < da6.len; i++) {
         struct DynArr* item = ar_get(da6, i);
         ar_del(*item);
